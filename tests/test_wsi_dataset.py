d59a4ed71f4dccc54935145d52cf2ee21bbb2876
BorisOne <boris95714@gmail.com>
2020-09-11 16:37:12 +0300

    added type checking

--- tests/test_wsi_dataset.py
+++ tests/test_wsi_dataset.py
@@ -1,4 +1,9 @@
-from wsi.dataset import (
+import sys
+from pathlib import Path
+FILIMDB_PATH = Path(__file__).resolve().parent.parent
+sys.path.append(str(FILIMDB_PATH))
+
+from wsi.score import (
     BTSRNC,
     load_bts_rnc_dataset,
     load_russe_labels,
@@ -9,17 +14,23 @@ def test_load_bts_rnc_dataset():
     part2data, language = load_bts_rnc_dataset(BTSRNC)
     assert "train" in part2data, f"Train part was not loaded"
     assert "test" in part2data, f"Test part was not loaded"
-    context_idxs, target_words, tokens_lists, target_idxs = part2data["train"]
-    assert len(target_words) == 3491
-    assert len(target_words) == len(tokens_lists)
-    assert len(tokens_lists) == len(target_idxs)
-    assert len(target_idxs) == len(context_idxs)
 
-    context_idxs, target_words, tokens_lists, target_idxs = part2data["test"]
-    assert len(target_words) == 6556
-    assert len(target_words) == len(tokens_lists)
-    assert len(tokens_lists) == len(target_idxs)
-    assert len(target_idxs) == len(context_idxs)
+    def _check_types(_context_idxs, _target_words, _contexts, _target_positions):
+        assert all(isinstance(idx, int) for idx in _context_idxs)
+        assert all(isinstance(word, str) for word in _target_words)
+        assert all(isinstance(ctx, str) for ctx in _contexts)
+        assert all(
+            isinstance(pos, str) and len(pos.split(",")) == 1
+            for pos in _target_positions
+        )
+
+    context_idxs, target_words, contexts, target_positions = part2data["train"]
+    assert 3491 == len(target_words) == len(contexts) == len(target_positions) == len(context_idxs)
+    _check_types(context_idxs, target_words, contexts, target_positions)
+
+    context_idxs, target_words, contexts, target_positions = part2data["test"]
+    assert 6556 == len(target_words) == len(contexts) == len(target_positions) == len(context_idxs)
+    _check_types(context_idxs, target_words, contexts, target_positions)
 
 
 def test_load_russe_labels():
