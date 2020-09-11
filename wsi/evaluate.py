21dfb681d784edf301d5a6c572ecec824ee6ea2a
BorisOne <boris95714@gmail.com>
2020-09-11 13:08:20 +0300

    removed dataset.py and dataset methods moved to score.py

--- wsi/evaluate.py
+++ wsi/evaluate.py
@@ -2,9 +2,8 @@ import logging
 from fire import Fire
 from pathlib import Path
 
-from wsi.dataset import load_dataset, save_preds
-from wsi.clusterizer import cluster_sentences
-from wsi.score import score_preds
+from clusterizer import cluster_sentences
+from score import score_preds, load_dataset, save_preds
 
 
 def evaluate(dataset: str = "bts-rnc"):
