ab6a6b8f848e509fe2b292e3bc1202f1dc44aeb7
narefyev <ezhick179@gmail.com>
2021-03-12 22:38:22 +0300

    sending train_unlabeled to the classifier without saving it to preds.tsv

--- evaluate.py
+++ evaluate.py
@@ -15,10 +15,9 @@ def pretrain(ds_name, module, part2xy, transductive):
     train_ids, train_texts, train_labels = part2xy['train']
     
     _, train_unlabeled_texts, _ = score.load_dataset_fast(ds_name, parts=('train_unlabeled',))['train_unlabeled']
-    part2xy['unlabeled'] = (None, train_unlabeled_texts, None)
 
     if transductive:
-        all_texts = list(text for _, text, _ in part2xy.values())
+        all_texts = [text for _, text, _ in part2xy.values()] + [train_unlabeled_texts]
     else:
         all_texts = [train_texts, train_unlabeled_texts]
 
